a3e9def4f2f676f9fe3cc6454012be3dd4d999b8
mdondrup <mdondrup@gmail.com>
2014-04-11 14:56:36 +0200

    edited documentation, removed unnecessary javascript code inclusion

--- conf/plugins/SAMLAuthenticator.pm
+++ conf/plugins/SAMLAuthenticator.pm
@@ -3,13 +3,10 @@ package Bio::Graphics::Browser2::Plugin::SAMLAuthenticator;
 use strict;
 use warnings;
 use Net::SAML;
-use CGI qw (:standard);
+use CGI::Fast qw (:standard);
 use URI::Escape;
 use Data::Dumper;
 use base 'Bio::Graphics::Browser2::Plugin::AuthPlugin';
-
-
-use constant DEBUG => 1;
 
 =pod
 
@@ -96,46 +93,54 @@ michael.dondrup <at> ii.uib.no
 =cut
 
 
+
+
+
+
 sub authenticate {
  my ($self, $samlart) = @_;
 
  my $idp = $self->setting('idp');
  my $mandatoryRole = $self->setting('mandatory role');
- ## IdP is required in the configuration file
  die "missing IDP" unless $idp;
  my $url = $self->setting('url') || url(-path_info=>1);
- warn "URL: $url" if DEBUG;
- warn "mandatory Role: $mandatoryRole" if DEBUG;
- # die "missing role" unless $mandatoryRole; # shouldn't require a role
+ warn "URL: $url";
+ warn "mandatory Role: $mandatoryRole";
+ #die "missing role" unless $mandatoryRole;
  return _doSSO($samlart, $idp, $url, $mandatoryRole);
 }
 
 sub authentication_hint {
-
-  return "SAML Authentication"
+  return "Acme Corp Single Sign-on"
 }
 
+
 ### SAML authentication needs to send http redirect to the IdP's address
-### This is only possible, if the plugin gets full controll over the http-header
-### If this hook doesn't exist of returns nothing, the normal plugin login should 
-### take effect.
+#### This is only possible, if the plugin gets full controll over the http-header
+#### If this hook doesn't exist of returns nothing, the normal plugin login should 
+#### take effect.
 
 sub redirect_header_hook {
-
-  unless (param('action') eq "sso_authenticate") { # avoid redirect loop!
+  unless (param('action') && param('action') eq "sso_authenticate") { # avoid redirect loop!
     my $action = "?action=sso_authenticate";
     if (param('SAMLart')) {
       # not sure if uri_escape is necessary here
       # it is required at least once, because Net::SAML doesn't 
       # do it automatically
       $action .= "&SAMLart=".uri_escape(param('SAMLart'));
-    }
-    
+    }    
     return ($action)
   }
 }
 
 sub configure_form {
+return 
+<<HTML
+<p>redirecting to IdP...</p>
+<script type="text/javascript">
+window.location = "?action=sso_authenticate";
+</script>
+HTML
 }
 
 
@@ -143,54 +148,73 @@ sub _doSSO {
   my $samlart = shift;
   my $idp = uri_escape(shift);
 
-  my $q = new CGI;
   my $url = shift;  # Edit to match your situation
   my $mandatoryRole = shift;
-  my $conf = "URL=$url&";
+  my $conf = "PATH=/home/licebase/zxid/var/zxid/&URL=$url&";
   my $cf = Net::SAML::new_conf_to_cf($conf);
-  
+  print STDERR "QUERY string from CGI: ".query_string();
+  warn "got a SAML response" if param('SAMLResponse');	
  
-    my $qs = "";
+  my $qs = "";
   if ($samlart) {
     $qs = "SAMLart=$samlart";
   } else {
-    $qs = $ENV{'QUERY_STRING'};
-    $qs = <STDIN> if $qs =~ /o=P/;
-    
+    print STDERR "no samlartefact, trying to read the query string\n";
+  #  $qs = $ENV{'QUERY_STRING'};
+  #  print STDERR "QS from ENV:$qs";
+  #  print STDERR "RECEIVED POSTDATA" if  $qs =~ /o=P/;
+    $qs = query_string();
+    #$qs = query_string() if $qs =~ /o=P/;
+    #print STDERR "QS from CGI:$qs"; 
     $qs .= "&e=$idp&l0=TRUE"
       unless ($qs =~ /s=|(SAMLart=)|o=B/);
   }
   
-  #print STDERR "QS1: $qs\n";
-  #  return;
+  print STDERR "final QS passed to netsaml: $qs\n";
+  
   my $res = Net::SAML::simple_cf($cf, -1, $qs, undef, 0x1828); # keep the flags 0x1828 !!!
-  #print STDERR "QS2: ",$ENV{'QUERY_STRING'},"\n";
-#  print STDERR "RESULT: $res\n";
+  
+  #  print STDERR "RESULT: $res\n";
   my $op = substr($res, 0, 1);
- # print STDERR "OP: $op\n";
- # open TMP, ">>/tmp/samlout.xml" or die $!;
- # print  TMP "=======SAML=========\n$res\n=======END=SAML===========\n";
- # close TMP;
-  if ($op eq 'L' || $op eq 'C') { 
-   
-    print $res; return; } # LOCATION (Redir) or CONTENT
- # if ($op eq 'n') { exit; } # already handled
- # if ($op eq 'e') { exit; } # not logged in, should render select screen
+  # print STDERR "OP: $op\n";
+  
+  if ($op eq 'L' || $op eq 'C') {    
+      print $res; 
+      return; 
+  } # LOCATION (Redir) or CONTENT
+  # if ($op eq 'n') { exit; } # already handled
+  # if ($op eq 'e') { exit; } # not logged in, should render select screen
   if ($op ne 'd') { die "Unknown Net::SAML::simple() res($res)"; }
- # $op == d means logged in
-  my ($sid) = $res =~ /^sesid: (.*)$/m;  # Extract a useful attribute from SSO output
-  warn ("SAML SessionId: ",$sid) if DEBUG;
-  return (_parse_saml($res, $mandatoryRole), $sid);
+  # $op == d means logged in
+  return (_parse_saml($res, $mandatoryRole));
 
 }
-
 
 sub _parse_saml {
   my $saml = shift;
   my $mandatoryRole = shift;
+  my ($sid) = $saml =~ /^sesid: (.*)$/m;  # Extract a useful attribute from SSO output
+  return undef unless $sid;    
+  my %roles = map {$_,1} ($saml =~  m/^role:\s+(.+)$/mg);
+  print STDERR join "\t", %roles;
+  return undef if ($mandatoryRole && !$roles{$mandatoryRole});
+
+  my ($uid) = $saml =~ m/^uid:\s+(.+)$/m;
+  return undef unless $uid;
+  my ($displayName) = $saml =~ m/^displayName:\s+(.+)$/m;
+  my ($mail) = $saml =~ m/^mail:\s+(.+)$/m;
+
+  return ($uid, $displayName, $mail, $sid)
+}
+
+
+
+sub _parse_saml_woid {
+  my $saml = shift;
+  my $mandatoryRole = shift;
   
   my %roles = map {$_,1} ($saml =~  m/^urn:oid:1\.3\.6\.1\.4\.1\.5923\.1\.1\.1\.1:\s+(.+)$/mg);
-  print STDERR join "\t", %roles if DEBUG;
+  print STDERR join "\t", %roles;
   return undef if ($mandatoryRole && !$roles{$mandatoryRole});
 
   my ($uid) = $saml =~ m/^urn:oid:0\.9\.2342\.19200300\.100\.1\.1:\s+(.+)$/m;
@@ -202,9 +226,13 @@ sub _parse_saml {
   return ($uid, $displayName, $mail)
 }
 
+
+
 sub plugin_logout_hook {
-  ## I guess the whole stuff should go into a hook in the authetication module
-  ## REFACTOR: create a hook logout_request_hook in SAMLauthenticator plugin.... done
+  
+  # I guess the whole stuff should go into a hook in the authetication module
+  ## REFACTOR: create a hook logout_request_hook in SAMLauthenticator plugin
+  warn "plugin_logout_hook called!\n";
   my $self = shift;
   my $action = shift;
   my $q    = shift;
@@ -215,38 +243,44 @@ sub plugin_logout_hook {
   my $sessionid = $session->id;
   my $username  = $session->username;
   my $openid    = $session->using_openid;
-  my $qs = $ENV{'QUERY_STRING'};
+  #my $qs = query_string();
   my $sid = $session->samlid();
-
   # Close the GBrowse session, before terminating the master session:
   ## QUESTION: I don't really know how to validly close the authenticated GBrowse session!
   ## What is the correct way of doing this?
-  ## ICICIC: that's possibly a loophole
   ## I found that a gbrowse request ?id=logout would close the GBrowse session, however I am not able
   ## to locate and modify the receiver (need to add code for SAML session termination).
   ## Here, I just remove the username
   $session->username("");
  # $session->id("");
   $session->flush();
- # $session->delete();
- # $session->flush();
+  #$session->delete();
+  #$session->flush();
 
  # removed hardcoded URL, done
-  my $url = $q->url().'/'.$globals->default_source.'/'; # "http://localhost:8888/cgi-bin/gb2/gbrowse/yeast/"
-  my $conf = "URL=$url";
+  my $url = $q->url().'/'.'yeast'.'/'; # "http://localhost:8888/cgi-bin/gb2/gbrowse/yeast/"
+  my $conf = "PATH=/home/licebase/zxid/var/zxid/&URL=$url";
+  warn "about to call Net::SAML::new_conf_to_cf\n";
   my $cf = Net::SAML::new_conf_to_cf($conf);
+  # warn "about to call Net::SAML::fetch_ses\n";
   # retrieve SAML session by id
-  my $ses = Net::SAML::fetch_ses($cf, $sid); 
+  my $ses = Net::SAML::fetch_ses($cf, $sid);
+  # warn "getting the cgi";
+  my $cgi = Net::SAML::new_cgi($cf, query_string());	 
   # generate the request to terminate the SAML session
-  my $redir =  Net::SAML::sp_slo_redir($cf, -1 ,$ses); 
+  #warn "about to call Net::SAML::sp_slo_redir : $ses \n";
+  my $redir =  Net::SAML::sp_slo_redir($cf, $cgi ,$ses); 
+  #  warn "GOT IT";
   # the result is the complete http header, plus some \r characters
   # but we need only the URL, so remove the rest (there seems to be no
   # not function in Net::SAML to receive the URL alone...)
   $redir =~ s/^Location: //;
   $redir =~ s/[\r|\n]//g;
+  warn "sending redirect for single logout:\n'$redir'";
   # return the redirect request
   return  (302, "text/html", $redir);
 }
+
 sub plugin_logout_action_hook {
   'action=plugin_logout';
 }
@@ -255,7 +289,7 @@ sub plugin_logout_action_hook {
 
 1;
 
-### Inject additional ACTION into the action package:
+### Inject additional ACTIONS into the action package:
 # Having this in the plugin pm only works with the 
 # data source that requires this plugin
 
@@ -270,7 +304,7 @@ package Bio::Graphics::Browser2::Action;
 
 
 ## This action is based on a copy of plugin_authenticate
-## It is meant to take controll over the SAML SSO process
+## It is meant to 
 sub ACTION_sso_authenticate {
   my $self = shift;
   my $q    = shift;
@@ -346,45 +380,20 @@ SCRIPT
 
 ## UGLY: Well this is realy ugly, also
 # ICICIC: hardcoded URL!
-my $html =	 <<HTML
+my $html =	 
+<<HTML
 <html>
 <head>
 <script src="/gbrowse2/js/login.js" type="text/javascript"></script> 
-
-<script src="?action=get_translation_tables;language=en-us" type="text/javascript"></script>
-<script src="/gbrowse2/js/prototype.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/scriptaculous.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/subtracktable.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/controls.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/autocomplete.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/login.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/buttons.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/trackFavorites.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/karyotype.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/rubber.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/overviewSelect.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/detailSelect.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/regionSelect.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/track.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/balloon.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/balloon.config.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/GBox.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/ajax_upload.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/tabs.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/track_configure.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/track_pan.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/ruler.js" type="text/javascript"></script>
 <script src="/gbrowse2/js/controller.js" type="text/javascript"></script>
-<script src="/gbrowse2/js/snapshotManager.js" type="text/javascript"></script>
-
- 
 </head>
 <body> \n
 $script
 <p>
 
 
-You will be redirected to GBrowse, if that doesn't happen automatically, <a href="http://localhost:8888/cgi-bin/gb2/gbrowse/yeast/">click here</a>
+You will be redirected to GBrowse, if that doesn't happen automatically, 
+<a href="http://localhost/cgi-bin/gb2/gbrowse/yeast/">click here</a>
 </p>
 </body>
 </html>
@@ -407,3 +416,4 @@ return  (200, "text/html", $html);
 
 
 __END__
+
